642af57930a988f64c57191556fd2c6c0f37eeb0
Leonam <leonnam.souza@gmail.com>
2022-12-07 14:25:12 -0300

    Contador de ações

---
+++ acoes.js
@@ -0,0 +1,17 @@
+let resp = window.document.getElementById('saida');
+
+function acao1(){
+    resp.innerHTML += '<p>Você clicou no primeiro botão.</p>'
+}
+
+function acao2(){
+    resp.innerHTML += '<p>Você clicou no segundo botão.</p>'
+}
+
+function acao3(){
+    resp.innerHTML += '<p>Você clicou no terceiro botão.</p>'
+}
+
+function acao4(){
+    resp.innerHTML += '<p>Você clicou no quarto botão.</p>'
+}
